4dfd737848cf708365d463f49af995bdbb9f9a67
Ruslan <specter63@gmail.com>
2023-02-22 01:36:47 +0400

    EventsCountConfirmed

--- main/src/main/java/ru/practicum/explore_with_me/model/event/EventsCountConfirmed.java
+++ main/src/main/java/ru/practicum/explore_with_me/model/event/EventsCountConfirmed.java
@@ -1,14 +1,12 @@
 package ru.practicum.explore_with_me.model.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class EventsCountConfirmed {
     private Long id;
     private Integer count;
 
-    public EventsCountConfirmed(Long id, Integer count) {
-        this.id = id;
-        this.count = count;
-    }
 }
